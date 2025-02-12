c57987b06b5b838a60fb49dedb5a488d8eb71c1d
HergenEngelhardt <168177848+HergenEngelhardt@users.noreply.github.com>
2025-02-12 10:30:56 +0100

    clouds are moving

--- classes/clouds.class.js
+++ classes/clouds.class.js
@@ -5,11 +5,21 @@ class Clouds extends MovableObject{
         super().loadImage('/assets/img/5_background/layers/4_clouds/1.png');
 
         this.x = -100 + Math.random() * 800;
+        this.animate();
         this.y = 0;
         this.width = 500;
         this.height = 250;
 
     }   
 
+
+    animate() {
+        setInterval(() => {
+            this.x -= 0.25;
+            if (this.x < -500) {
+                this.x = 800;
+            }
+        }, 1000 / 60)
+    }
 
 }
