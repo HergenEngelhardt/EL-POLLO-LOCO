5cc4c702d3491171676afdfbfc3a8988aad26980
HergenEngelhardt <168177848+HergenEngelhardt@users.noreply.github.com>
2025-02-12 11:37:17 +0100

    animated character and chicken

--- classes/character.class.js
+++ classes/character.class.js
@@ -2,8 +2,29 @@ class Character extends MovableObject {
     y = 200;
     width = 150;
     height = 250;
+    IMAGES_WALKING = [ 
+        'assets/img/2_character_pepe/2_walk/W-21.png',
+        'assets/img/2_character_pepe/2_walk/W-22.png',
+        'assets/img/2_character_pepe/2_walk/W-23.png',
+        'assets/img/2_character_pepe/2_walk/W-24.png',
+        'assets/img/2_character_pepe/2_walk/W-25.png',
+        'assets/img/2_character_pepe/2_walk/W-26.png'
+    ];
+
     constructor() {
         super().loadImage('/assets/img/2_character_pepe/1_idle/idle/I-1.png');
+        this.loadImages(this.IMAGES_WALKING);
+
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 500);
     }
 
     jump() {
--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -1,11 +1,26 @@
 class Chicken extends MovableObject{
-    y = 340;
-    height = 100;
-    width = 80;
+    y = 365;
+    height = 80;
+    width = 50;
+    IMAGES_WALKING_CHICKEN = [ 
+        'assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+        'assets/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+        'assets/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+    ];
+
     constructor() {
         super().loadImage('/assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.x = 250 + Math.random() * 500;
-    }   
+        this.loadImages(this.IMAGES_WALKING_CHICKEN);
+        this.animate();
+    }
 
-
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING_CHICKEN.length;
+            let path = this.IMAGES_WALKING_CHICKEN[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 200);
+    }
 }
