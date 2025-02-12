3816150e36ca186b7b776c3dfcedd129116a86f3
HergenEngelhardt <168177848+HergenEngelhardt@users.noreply.github.com>
2025-02-12 19:19:14 +0100

    camera is moving

--- classes/world.class.js
+++ classes/world.class.js
@@ -15,7 +15,7 @@ class World {
     canvas;
     ctx;
     keyboard;
-
+    camera_x = - 0;
 
     constructor(canvas, keyboard) {
         this.keyboard = keyboard;
@@ -31,11 +31,15 @@ class World {
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+        this.ctx.translate(this.camera_x, 0);
         this.addObjectsToMap(this.background);
         this.addToMap(this.character)
 
         this.addObjectsToMap(this.enemies);
         this.addObjectsToMap(this.cloud);   
+
+
+        this.ctx.translate(-this.camera_x, 0);
 
         let self = this;
         requestAnimationFrame(function() {
