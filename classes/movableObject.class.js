6523fde9bb2998d9d7d98cf03d9290e643f8458f
HergenEngelhardt <168177848+HergenEngelhardt@users.noreply.github.com>
2025-02-17 12:50:21 +0100

    instanceof

--- classes/movableObject.class.js
+++ classes/movableObject.class.js
@@ -29,6 +29,20 @@ class MovableObject {
         this.img.src =  path;
     }
 
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+    }
+
+    drawFrame(ctx) {
+
+        if(this instanceof Character || this instanceof Chicken || this instanceof ChickenBoss || this instanceof SalsaBottle || this instanceof Coin){
+        ctx.beginPath();
+        ctx.lineWidth = "3";
+        ctx.strokeStyle = "red";
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
+    }}
+
     loadImages(arr) {
         arr.forEach((path) => {
             let img = new Image();
