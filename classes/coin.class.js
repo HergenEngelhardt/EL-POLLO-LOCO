2e10dda16ae7467be46c0cea45af503ccf5bf727
HergenEngelhardt <168177848+HergenEngelhardt@users.noreply.github.com>
2025-02-19 14:15:46 +0100

    coin are at diffenrent height

--- classes/coin.class.js
+++ classes/coin.class.js
@@ -2,7 +2,7 @@ class Coin extends MovableObject {
     constructor() {
         super().loadImage('/assets/img/8_coin/coin_1.png');
         this.x = 250 + Math.random() * 1000;
-        this.y = 300;
+        this.y = 300 - Math.random() *  375;
         this.height = 180;
         this.width = 150;
     }
