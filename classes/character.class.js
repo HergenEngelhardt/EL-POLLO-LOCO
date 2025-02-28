bccc3547427dbb402f1ee76b721d2d56749721a9
HergenEngelhardt <168177848+HergenEngelhardt@users.noreply.github.com>
2025-02-28 13:43:41 +0100

    idle added

--- classes/character.class.js
+++ classes/character.class.js
@@ -4,6 +4,34 @@ class Character extends MovableObject {
     height = 250;
     speed = 4;
     deadAnimationPlayed = false;
+    lastMoveTime = Date.now();
+
+    IMAGES_IDLE = [ 
+        'assets/img/2_character_pepe/1_idle/idle/I-1.png',
+        'assets/img/2_character_pepe/1_idle/idle/I-2.png',
+        'assets/img/2_character_pepe/1_idle/idle/I-3.png',
+        'assets/img/2_character_pepe/1_idle/idle/I-4.png',
+        'assets/img/2_character_pepe/1_idle/idle/I-5.png', 
+        'assets/img/2_character_pepe/1_idle/idle/I-6.png',
+        'assets/img/2_character_pepe/1_idle/idle/I-7.png',
+        'assets/img/2_character_pepe/1_idle/idle/I-8.png',
+        'assets/img/2_character_pepe/1_idle/idle/I-9.png',
+        'assets/img/2_character_pepe/1_idle/idle/I-10.png'
+    ];
+
+    IMAGES_IDLE_LONG = [ 
+        'assets/img/2_character_pepe/1_idle/long_idle/I-11.png',
+        'assets/img/2_character_pepe/1_idle/long_idle/I-12.png',
+        'assets/img/2_character_pepe/1_idle/long_idle/I-13.png',
+        'assets/img/2_character_pepe/1_idle/long_idle/I-14.png',
+        'assets/img/2_character_pepe/1_idle/long_idle/I-15.png',
+        'assets/img/2_character_pepe/1_idle/long_idle/I-16.png',
+        'assets/img/2_character_pepe/1_idle/long_idle/I-17.png',
+        'assets/img/2_character_pepe/1_idle/long_idle/I-18.png',
+        'assets/img/2_character_pepe/1_idle/long_idle/I-19.png',
+        'assets/img/2_character_pepe/1_idle/long_idle/I-20.png'
+    ];
+
     IMAGES_WALKING = [ 
         '/assets/img/2_character_pepe/2_walk/W-21.png',
         '/assets/img/2_character_pepe/2_walk/W-22.png',
@@ -43,7 +71,9 @@ class Character extends MovableObject {
     ]; 
 
     constructor() {
-        super().loadImage(this.IMAGES_WALKING[0]);
+        super().loadImage(this.IMAGES_IDLE[0]); 
+        this.loadImages(this.IMAGES_IDLE);
+        this.loadImages(this.IMAGES_IDLE_LONG);
         this.loadImages(this.IMAGES_WALKING);
         this.loadImages(this.IMAGES_JUMPING);
         this.loadImages(this.IMAGES_DEAD);
@@ -61,15 +91,18 @@ class Character extends MovableObject {
                 if(this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x){
                     this.moveRight();
                     this.otherDirection = false;
+                    this.lastMoveTime = Date.now();
                 }
                 
                 if(this.world.keyboard.LEFT && this.x > 0){
                     this.moveLeft();
                     this.otherDirection = true;
+                    this.lastMoveTime = Date.now();
                 }
 
                 if(this.world.keyboard.JUMP && !this.isAboveGround()){
                     this.jump();
+                    this.lastMoveTime = Date.now();
                 }
 
                 this.world.camera_x = -this.x;
@@ -89,6 +122,10 @@ class Character extends MovableObject {
             } else {
                 if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT){
                     this.animateImages(this.IMAGES_WALKING);
+                } else if (Date.now() - this.lastMoveTime > 5000) {
+                    this.animateImages(this.IMAGES_IDLE_LONG);
+                } else {
+                    this.animateImages(this.IMAGES_IDLE);
                 }
             }
         }, 100);
